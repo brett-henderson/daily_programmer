20a07524286091f73812adf5e7fa3cd0541f97c1
Brett Henderson <brett.henderson@bremily.net>
2019-03-24 18:59:46 +1000

    add file

---
+++ Challenge #375 [Easy] Print a new number by adding one to each of its digit/increment_digits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+# File:		increment_digits.py
+# Author:	Brett Henderson
+# Contact:	brett.henderson@bremily.net
+# Created:	2019-03-24
+#
+# Purpose:	Take a number from the user and print a new number by incrementing each digit by 1.
+
+def increment_digits(number):
+	ans = ''
+	for digit in number:
+		ans += str(int(digit) + 1)
+	return ans
+
+number = (input('Enter a number: '))
+new_number = increment_digits(number)
+
+print()
+print('Old number: ' + number)
+print('New number: ' + new_number)
+
