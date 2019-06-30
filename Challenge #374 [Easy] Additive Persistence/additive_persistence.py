30b154adc6395ceba532624da01c38f1a2012f18
Brett Henderson <brett.henderson@bremily.net>
2019-06-30 18:01:25 +1000

    Modified

---
+++ Challenge #374 [Easy] Additive Persistence/additive_persistence.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# File:         additive_persistence.py
+# Author:       Brett Henderson
+# Contact:      brett.henderson@bremily.net
+# Created:      2019-06-03
+#
+# Purpose:      Perform "additive persistence" by iteratively summing the digits of a number until
+#               only 1 digit remains
+
+# INCOMPLETE #
+def additive_persistence(number):
+    return ""
+
+number = input("Enter a number: ")
+answer = additive_persistence(number)
+print(number + ' -> ' + answer)
