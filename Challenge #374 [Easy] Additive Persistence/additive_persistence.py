9bb1a283326382487a373ff826f960e3287b4c9c
Brett Henderson <brett.henderson@bremily.net>
2020-01-25 15:46:05 +1000

    Fix incomplete program

--- Challenge #374 [Easy] Additive Persistence/additive_persistence.py
+++ Challenge #374 [Easy] Additive Persistence/additive_persistence.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
 
-# INCOMPLETE #
 def additive_persistence(number):
-    return ""
+    # Loop backwards through each digit of the number and
+    # return the sum of its digits
+    add = 0
+    for i in range(len(number)-1,-1,-1):
+        add += int(number[i])
+    return str(add)
 
 number = input("Enter a number: ")
-answer = additive_persistence(number)
-print(number + ' -> ' + answer)
+answer = number # Keep number unchanged for final output
+count = 0 # The additive persistence value
+
+# The number will change to the sum of its digits until that
+# sum is only 1 digit long
+while len(answer) > 1:
+    print(answer)
+    answer = additive_persistence(answer)
+    count += 1
+
+print(number + ' -> ' + str(count))
